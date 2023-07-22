632d9a61a45c14cd4cc54a6fc186bec74035e86d
Nikita <ba999@mail.ru>
2023-07-22 18:53:37 +0300

    added ComplexCalculator

---
+++ ComplexCalculate/src/Logger/Logger.java
@@ -0,0 +1,13 @@
+package Logger;
+
+/**
+ * Интерфейс Logger определяет метод для логирования сообщений.
+ */
+public interface Logger {
+    /**
+     * Записывает логовое сообщение.
+     *
+     * @param message Логовое сообщение.
+     */
+    void log(String message);
+}
